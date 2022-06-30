28270fb3d642799af1fb16836c66fc4eb2b13957
dnovs0416 <davenovales@gmail.com>
2022-06-29 22:15:07 -0400

    added class 2

---
+++ src/com/syntax/class02/HW1.java
@@ -0,0 +1,10 @@
+package com.syntax.class02;
+
+public class HW1 {
+     /*
+    navigate to https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22
+    fill out the form
+    click on register
+    close the browser
+     */
+}
---
+++ src/com/syntax/class02/HW2.java
@@ -0,0 +1,12 @@
+package com.syntax.class02;
+
+public class HW2 {
+   /*
+   navigate to fb.com
+    click on create new account
+    fill up all the textboxes
+    click on sign up button
+    close the pop up
+    close the browser
+    */
+}
