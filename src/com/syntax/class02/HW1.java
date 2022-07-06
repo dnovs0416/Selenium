6b3046e044ac13d4e207ed51960c386772793f54
dnovs0416 <davenovales@gmail.com>
2022-07-05 22:08:39 -0400

    added class 3

--- src/com/syntax/class02/HW1.java
+++ src/com/syntax/class02/HW1.java
@@ -1,5 +1,10 @@
 package com.syntax.class02;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HW1 {
      /*
     navigate to https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22
@@ -7,4 +12,36 @@ public class HW1 {
     click on register
     close the browser
      */
+     public static void main(String[] args) throws InterruptedException {
+         System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+         WebDriver driver=new ChromeDriver();
+
+         driver.get("https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22");
+         WebElement firstName = driver.findElement(By.id("customer.firstName"));
+         firstName.sendKeys("Homer");
+         WebElement lastName = driver.findElement(By.id("customer.lastName"));
+         lastName.sendKeys("Simpson");
+         WebElement address = driver.findElement(By.id("customer.address.street"));
+         address.sendKeys("123 Easy Street");
+         WebElement city = driver.findElement(By.name("customer.address.city"));
+         city.sendKeys("Springfield");
+         WebElement state = driver.findElement(By.name("customer.address.state"));
+         state.sendKeys("IL");
+         WebElement zipcode = driver.findElement(By.id("customer.address.zipCode"));
+         zipcode.sendKeys("46466");
+         WebElement phoneNo = driver.findElement(By.id("customer.phoneNumber"));
+         phoneNo.sendKeys("123-456-7890");
+         WebElement ssn = driver.findElement(By.name("customer.ssn"));
+         ssn.sendKeys("123-45-6789");
+         WebElement username = driver.findElement(By.name("customer.username"));
+         username.sendKeys("theSimpson");
+         WebElement password = driver.findElement(By.id("customer.password"));
+         password.sendKeys("password");
+         WebElement confirm = driver.findElement(By.name("repeatedPassword"));
+         confirm.sendKeys("password");
+
+         driver.findElement(By.xpath("//input[@type='submit'][@value='Register']")).click();
+         Thread.sleep(5000);
+         driver.quit();
+     }
 }
--- src/com/syntax/class02/HW2.java
+++ src/com/syntax/class02/HW2.java
@@ -1,5 +1,11 @@
 package com.syntax.class02;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class HW2 {
    /*
    navigate to fb.com
@@ -9,4 +15,27 @@ public class HW2 {
     close the pop up
     close the browser
     */
+   public static void main(String[] args) throws InterruptedException {
+       System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+       WebDriver driver = new ChromeDriver();
+       driver.navigate().to("https://www.fb.com");
+       driver.findElement(By.linkText("Create new account")).click();
+       Thread.sleep(2000);
+       WebElement firstName = driver.findElement(By.name("firstname"));
+       firstName.sendKeys("Peter");
+       WebElement lastName = driver.findElement(By.name("lastname"));
+       lastName.sendKeys("Griffin");
+       WebElement email = driver.findElement(By.name("reg_email__"));
+       email.sendKeys("family_guy@yahoo.com");
+       WebElement emailConfirm = driver.findElement(By.name("reg_email_confirmation__"));
+       emailConfirm.sendKeys("family_guy@yahoo.com");
+       WebElement password = driver.findElement(By.id("password_step_input"));
+       password.sendKeys("password");
+
+       driver.findElement(By.xpath("//button[@type='submit'][text()='Sign Up']")).click();
+       Thread.sleep(1500);
+       driver.findElement(By.xpath("//img[@class='_8idr img'][contains(@src,'https://static.xx.fbcdn.net/')]")).click();
+       Thread.sleep(1000);
+       driver.quit();
+   }
 }
